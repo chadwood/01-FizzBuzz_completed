37f04c8f896fbc3110144ef79fe2546e53878e83
chad.wood <chad.wood@external.cnhind.com>
2021-12-17 16:47:57 -0500

    first commit

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Katas
+{
+    public class FizzBuzz
+    {
+        private const int MinInputValue = 1;
+        private const int MaxInputValue = 100;
+
+        private const string FizzName = "Fizz";
+        private const int FizzDivisor = 3;
+
+        private const string BuzzName = "Buzz";
+        private const int BuzzDivisor = 5;
+
+        static void Main(string[] args) {
+
+		}
+
+        public string Process(int inputValue) {
+            ValidateInputValue(inputValue);
+            var _retVal = $"{PrintFizz(inputValue)}{PrintBuzz(inputValue)}";
+            return string.IsNullOrEmpty(_retVal) ? inputValue.ToString() : _retVal;
+        }
+
+        private void ValidateInputValue(int inputValue) {
+            if (!IsBetweenMinMaxValue(inputValue)) {
+                throw new ArgumentOutOfRangeException($"Only integers {MinInputValue} through {MaxInputValue} are allowed.");
+            }
+        }
+
+        private bool IsBetweenMinMaxValue(int inputValue) {
+            return inputValue >= MinInputValue && inputValue <= MaxInputValue;
+        }
+
+        private string PrintFizz(int inputValue) {
+            return Modulo(inputValue, FizzDivisor) ? FizzName : string.Empty;
+        }
+
+        private string PrintBuzz(int inputValue) {
+            return Modulo(inputValue, BuzzDivisor) ? BuzzName : string.Empty;
+        }
+
+        private bool Modulo(int dividend, int divisor) {
+            return dividend % divisor == 0;
+        }
+
+    }
+}
---
+++ FizzBuzzTest/FizzBuzzTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Katas
+{
+    [TestFixture]
+    public class NUnitTest1
+    {
+        [Test]
+        public void ShouldReturn1For1() {
+            FizzBuzz _fizzBuzz = new FizzBuzz();
+
+            //Values are not a multiple of 3 or 5
+            Assert.AreEqual("1", _fizzBuzz.Process(1));
+            Assert.AreEqual("4", _fizzBuzz.Process(4));
+            Assert.AreEqual("7", _fizzBuzz.Process(7));
+        }
+
+        [Test]
+        public void ShouldTriggerError()
+        {
+            FizzBuzz _fizzBuzz = new FizzBuzz();
+
+            //Value is > 100
+            Assert.Catch<ArgumentOutOfRangeException>(() => _fizzBuzz.Process(101));
+            Assert.Catch<ArgumentOutOfRangeException>(() => _fizzBuzz.Process(102));
+            Assert.Catch<ArgumentOutOfRangeException>(() => _fizzBuzz.Process(105));
+
+            //Value is < 100
+            Assert.Catch<ArgumentOutOfRangeException>(() => _fizzBuzz.Process(0));
+            Assert.Catch<ArgumentOutOfRangeException>(() => _fizzBuzz.Process(-3));
+            Assert.Catch<ArgumentOutOfRangeException>(() => _fizzBuzz.Process(-5));
+        }
+
+        [Test]
+        public void ShouldReturnFizz() {
+            var _fizzBuzz = new FizzBuzz();
+            Assert.AreEqual("Fizz", _fizzBuzz.Process(3));
+            Assert.AreEqual("Fizz", _fizzBuzz.Process(18));
+            Assert.AreEqual("Fizz", _fizzBuzz.Process(66));
+            Assert.AreEqual("Fizz", _fizzBuzz.Process(42));
+        }
+
+        [Test]
+        public void ShouldReturnBuzz()
+        {
+            var _fizzBuzz = new FizzBuzz();
+            Assert.AreEqual("Buzz", _fizzBuzz.Process(5));
+            Assert.AreEqual("Buzz", _fizzBuzz.Process(50));
+            Assert.AreEqual("Buzz", _fizzBuzz.Process(70));
+            Assert.AreEqual("Buzz", _fizzBuzz.Process(100));
+        }
+
+        [Test]
+        public void ShouldReturnFizzBuzz()
+        {
+            var _fizzBuzz = new FizzBuzz();
+            Assert.AreEqual("FizzBuzz", _fizzBuzz.Process(15));
+            Assert.AreEqual("FizzBuzz", _fizzBuzz.Process(30));
+            Assert.AreEqual("FizzBuzz", _fizzBuzz.Process(45));
+            Assert.AreEqual("FizzBuzz", _fizzBuzz.Process(75));
+            Assert.AreEqual("FizzBuzz", _fizzBuzz.Process(90));
+        }
+    }
+}
